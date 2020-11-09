e130a64335fc431859cafc01efa2fe6d92142b7e
eea15 <eea21@pitt.edu>
2020-11-09 13:14:42 -0500

    initial app boilerplate

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-
+# pypiwin32==219
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
